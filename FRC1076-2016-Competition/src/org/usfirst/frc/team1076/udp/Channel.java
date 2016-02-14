72215194f437028c0fd00238a6d8e56754436fde
Clayton Riegger <jonfieldthe2nd@gmail.com>
2016-02-14 15:43:03 -0500

    Add UDP communications class
    It can send and receive messages on a port and stores messages in a
    queue system. Messages are stored with the IP address of the receiver.

---
+++ FRC1076-2016-Competition/src/org/usfirst/frc/team1076/udp/Channel.java
@@ -0,0 +1,82 @@
+package org.usfirst.frc.team1076.udp;
+import java.net.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Channel {
+	private int port;
+	private InetAddress receiverIP;
+	private DatagramSocket serverSocket;
+	private Thread receiveWorker;
+	private boolean doesReceive = true;
+	private Queue<UDPMessage> queue;
+	
+	public Channel(int port) throws Exception {
+		this.queue = new LinkedList<UDPMessage>();
+		this.port = port;
+		this.serverSocket = new DatagramSocket(port);
+		this.receiveWorker = new Thread(new Runnable() {
+			private Channel containerChannel;
+			
+			private boolean shouldReceive() {
+				return this.containerChannel.getReceiveStatus();
+			}
+			
+			public Runnable init(Channel containerChannel) {
+				this.containerChannel = containerChannel;
+				return this;
+			}
+			
+			public void run() {
+				while (true) {
+					if (!this.shouldReceive()) continue;
+					DatagramPacket receivePacket = new DatagramPacket(new byte[1024], 1024);
+					try {
+						serverSocket.receive(receivePacket);
+						containerChannel.setIP(receivePacket.getAddress());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					String data = new String(receivePacket.getData());
+					data = data.substring(0, receivePacket.getLength());
+					containerChannel.putMessage(new UDPMessage(data, receivePacket.getAddress()));
+				}
+			}
+		}.init(this));
+		this.receiveWorker.start();
+	}
+	
+	public InetAddress getIP() {
+		return this.receiverIP;
+	}
+	
+	public void setIP(InetAddress newIP) {
+		this.receiverIP = newIP;
+	}
+	
+	protected boolean getReceiveStatus() {
+		return doesReceive;
+	}
+	
+	public void setReceiveStatus(boolean receive) {
+		doesReceive = receive;
+	}
+
+	public void putMessage(UDPMessage str) {
+		queue.add(str);
+	}
+	
+	public UDPMessage popLatestMessage() {
+		if(queue.isEmpty())
+			return null;
+		return queue.poll();
+	}
+	
+	public void sendMessage(String message) throws Exception {
+		serverSocket.send(new UDPMessage(message, this.receiverIP).sendPacket(5880));
+	}
+	
+	public void close() {
+		serverSocket.close();
+	}
+}
