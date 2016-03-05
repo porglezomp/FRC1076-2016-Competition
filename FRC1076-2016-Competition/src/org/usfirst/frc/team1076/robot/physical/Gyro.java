82b92d3b19abb0bff970ffb26045cc108e8b67fd
Caleb Jones <self@calebjones.net>
2016-03-04 20:23:23 -0500

    Add Gyro interface and physical implementation

---
+++ FRC1076-2016-Competition/src/org/usfirst/frc/team1076/robot/physical/Gyro.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team1076.robot.physical;
+
+import org.usfirst.frc.team1076.robot.sensors.IGyro;
+import edu.wpi.first.wpilibj.GyroBase;
+
+public class Gyro implements IGyro {
+	
+	GyroBase gyro;
+	
+	public Gyro(GyroBase gyro) {
+		this.gyro = gyro;
+	}
+	
+	@Override
+	public double getAngle() {
+		return gyro.getAngle();
+	}
+	
+	@Override
+	public double getRate() {
+		return gyro.getRate();
+	}
+
+	@Override
+	public void reset() {
+		gyro.reset();
+	}
+	
+}
