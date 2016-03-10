69a85f31682fcb26c7ff8ff92bbbd0ff99d46e71
Clayton Riegger <jonfieldthe2nd@gmail.com>
2016-03-09 21:02:49 -0500

    Outline the command chain for each SmartDashboard input

--- FRC1076-2016-Competition/src/org/usfirst/frc/team1076/robot/physical/AutoSelector.java
+++ FRC1076-2016-Competition/src/org/usfirst/frc/team1076/robot/physical/AutoSelector.java
@@ -3,6 +3,7 @@ package org.usfirst.frc.team1076.robot.physical;
 import org.usfirst.frc.team1076.robot.IRobot;
 import org.usfirst.frc.team1076.robot.sensors.IAutoSelector;
 import org.usfirst.frc.team1076.robot.statemachine.AngleAutonomous;
+import org.usfirst.frc.team1076.robot.statemachine.DistanceAutonomous;
 import org.usfirst.frc.team1076.robot.statemachine.ForwardAutonomous;
 import org.usfirst.frc.team1076.robot.statemachine.AutoState;
 import org.usfirst.frc.team1076.robot.statemachine.NothingAutonomous;
@@ -31,18 +32,56 @@ public class AutoSelector implements IAutoSelector {
 			if (!backwards) {
 				chain.setNext(new ForwardAutonomous(100, 0.2))
 					.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()));
+				// TODO: Figure out actual angles.
 			} else {
 				chain.setNext(new ForwardAutonomous(100, 0.2));
 			}
-			
+			chain.setNext(new UDPAutonomous(robot.getSensorData(), FieldPosition.Left))
+				// TODO: Make LIDARAutonomous class.
+				.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()))
+				// TODO: Figure out actual angles.
+				.setNext(null); 
+				// TODO: Replace with an Autonomous that drives forward until it reaches the goal.
 		case 2:
-			new UDPAutonomous(robot.getSensorData(), FieldPosition.Left);
+			if (!backwards) {
+				chain.setNext(new ForwardAutonomous(100, 0.2))
+					.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()));
+			} else {
+				chain.setNext(new ForwardAutonomous(100, 0.2));
+			}
+			chain.setNext(new UDPAutonomous(robot.getSensorData(), FieldPosition.Left))
+				.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()))
+				.setNext(null);
 		case 3:
-			new UDPAutonomous(robot.getSensorData(), FieldPosition.Left);
+			if (!backwards) {
+				chain.setNext(new ForwardAutonomous(100, 0.2))
+					.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()));
+			} else {
+				chain.setNext(new ForwardAutonomous(100, 0.2));
+			}
+			chain.setNext(new UDPAutonomous(robot.getSensorData(), FieldPosition.Left))
+				.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()))
+				.setNext(null);
 		case 4:
-			new UDPAutonomous(robot.getSensorData(), FieldPosition.Right);
+			if (!backwards) {
+				chain.setNext(new ForwardAutonomous(100, 0.2))
+					.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()));
+			} else {
+				chain.setNext(new ForwardAutonomous(100, 0.2));
+			}
+			chain.setNext(new UDPAutonomous(robot.getSensorData(), FieldPosition.Right))
+				.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()))
+				.setNext(null);
 		case 5:
-			new UDPAutonomous(robot.getSensorData(), FieldPosition.Right);
+			if (!backwards) {
+				chain.setNext(new ForwardAutonomous(100, 0.2))
+					.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()));
+			} else {
+				chain.setNext(new ForwardAutonomous(100, 0.2));
+			}
+			chain.setNext(new UDPAutonomous(robot.getSensorData(), FieldPosition.Right))
+				.setNext(new AngleAutonomous(0.0, 0.0, robot.getSensorData().getGyro()))
+				.setNext(null);
 		default:
 			// Just nothing
 		}
