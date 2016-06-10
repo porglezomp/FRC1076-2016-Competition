e369bc31492c04c50ce5f80a2288dcbb0affe546
Caleb Jones <code@calebjones.net>
2016-06-10 18:24:31 -0400

    Change the pneumatic solenoid numbers

--- FRC1076-2016-Competition/src/org/usfirst/frc/team1076/robot/physical/Robot.java
+++ FRC1076-2016-Competition/src/org/usfirst/frc/team1076/robot/physical/Robot.java
@@ -66,8 +66,8 @@ public class Robot extends IterativeRobot implements IRobot {
 	Servo lidarServo = new Servo(0);
 	
 	Compressor compressor = new Compressor(0);
-	ISolenoid intakePneumatic = new OneSolenoid(1);
-	ISolenoid shifterPneumatic = new OneSolenoid(0);
+	ISolenoid intakePneumatic = new TwoSolenoid(1, 3);
+	ISolenoid shifterPneumatic = new TwoSolenoid(0, 2);
 	IDistanceEncoder encoder;
 	
 	IRobotController teleopController;
